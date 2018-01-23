a5f073c577b0f1a0616bfc4ec9f73e6a68b32af3
May Meow <themaymeow@gmail.com>
2018-01-23 23:22:12 +0100

    More tests

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ProjectsTest extends TestCase
 {
-    use DatabaseMigrations, DatabaseTransactions;
+    use DatabaseTransactions;
     // use DatabaseTransactions;
     /**
      * A basic test example.
@@ -28,16 +28,93 @@ class ProjectsTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function view()
+    public function testShow()
     {
         $user = factory(User::class)->create();
-        $project = factory(Project::class)->create;
+        $project = factory(Project::class)->create();
 
         $response = $this->actingAs($user)
             ->withSession(['asd' => 'dsa'])
             ->get('/projects/' . $project->id);
         $response->assertStatus(200);
+    }
 
-        $response->assertSee($project->name);
+    public function testIssues()
+    {
+        $user = factory(User::class)->create();
+        $project = factory(Project::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['asd' => 'dsa'])
+            ->get('/projects/' . $project->id . '/issues');
+        $response->assertStatus(200);
+    }
+
+    public function testCommits()
+    {
+        $user = factory(User::class)->create();
+        $project = factory(Project::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['asd' => 'dsa'])
+            ->get('/projects/' . $project->id . '/commits');
+        $response->assertStatus(200);
+    }
+
+    public function testBranches()
+    {
+        $user = factory(User::class)->create();
+        $project = factory(Project::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['asd' => 'dsa'])
+            ->get('/projects/' . $project->id . '/branches');
+        $response->assertStatus(200);
+    }
+
+    public function testTags()
+    {
+        $user = factory(User::class)->create();
+        $project = factory(Project::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['asd' => 'dsa'])
+            ->get('/projects/' . $project->id . '/tags');
+        $response->assertStatus(200);
+    }
+
+    public function testMergeRequests()
+    {
+        $user = factory(User::class)->create();
+        $project = factory(Project::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['asd' => 'dsa'])
+            ->get('/projects/' . $project->id . '/merge-requests');
+        $response->assertStatus(200);
+    }
+
+    public function testMergeRequestsCreate()
+    {
+        $user = factory(User::class)->create();
+        $project = factory(Project::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['asd' => 'dsa'])
+            ->get('/projects/' . $project->id . '/merge-requests/new');
+        $response->assertStatus(200);
+    }
+
+    public function testCreate()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['role' => 'show-project'])
+            ->get('/projects/create');
+        $response->assertStatus(200);
+
+        $response->assertSee('<h1 class="display-4">Create</h1>');
+        $response->assertSee('<button type="submit" class="btn btn-primary">Create project</button>');
     }
 }
