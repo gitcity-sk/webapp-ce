75cfc886a704033ca76f663b01984ab55abd004f
May <may+github@maymeow.click>
2018-01-24 16:49:27 +0100

    Add show action to profile controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Profile;
 
 class ProfilesController extends Controller
 {
-    //
+    public function show(Profile $profile)
+    {
+        return "add view template"
+    }
 }
