82cd0a1112ba5f4948d544a729baafa6de57247a
May Meow <themaymeow@gmail.com>
2018-01-27 11:09:12 +0100

    Fix test for Projects
    add profile to project factory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -6,7 +6,9 @@ $factory->define(App\Project::class, function (Faker $faker) {
     $name = $faker->sentence;
     return [
         'user_id' => function() {
-            return factory(App\User::class)->create()->id;
+            $u =  factory(App\User::class)->create();
+            $u->profile()->save(factory(App\Profile::class)->make());
+            return $u;
         },
         'name' => $name,
         'slug' => str_slug($name),
