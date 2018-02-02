19a16d238d67701278364dffcf352d43e6f228d6
May <may+github@maymeow.click>
2018-02-02 18:01:07 +0100

    Add permission seeder

---
+++ database/seeds/AdminUserSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Profile;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // todo
+    }
+}
