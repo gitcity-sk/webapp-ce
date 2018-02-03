c2d1ddfb910731c339f6ad887c35ec0008002f11
May Meow <themaymeow@gmail.com>
2018-02-03 16:33:03 +0100

    more tests

---
+++ database/factories/LabelFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Label::class, function (Faker $faker) {
+    return [
+        'title' =>  $faker->sentence,
+        'color' =>  $faker->sentence,
+        'description' =>  $faker->sentence
+    ];
+});
--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -5,18 +5,25 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Project;
 
 class ProjectTest extends TestCase
 {
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testUser()
+    public function testCreateProject()
     {
         $project = factory(Project::class)->create();
+
+        $foundProject = Project::find($project->id);
+
+        $this->assertEquals($foundProject->name, $project->name);
+        $this->assertEquals($foundProject->description, $project->description);
 
         $this->assertDatabaseHas('users', [
             'name' => $project->user->name
