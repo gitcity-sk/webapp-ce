ec27b57dee697964bdf4b22b2d6b36f5bbdd7205
May Meow <themaymeow@gmail.com>
2018-02-03 10:17:11 +0100

    test

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -5,9 +5,12 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
 
 class ExampleTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+    
     /**
      * A basic browser test example.
      *
@@ -17,7 +20,20 @@ class ExampleTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                    ->assertSee('Laravel');
+                    ->assertSee('Webapp');
+        });
+
+        $user = factory(User::class)->create([
+            'email' => 'taylor@laravel.com',
+        ]);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'secret')
+                    ->press('Login')
+                    ->assertPathIs('/dashboard')
+                    ->assertSee('Authorized keys');
         });
     }
 }
