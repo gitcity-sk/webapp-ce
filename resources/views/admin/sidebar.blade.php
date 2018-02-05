4792b19e238f37e1c24f5730ff096a1916b13ab6
May Meow <themaymeow@gmail.com>
2018-02-05 21:43:49 +0100

    show active links

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,7 +1,7 @@
-<ul class="top-level-items">
-    <li><a href="/admin/users" class="nav-link">Users</a></li>
-    <li><a href="/admin/roles" class="nav-link">Roles</a></li>
-    <li><a href="/admin/permissions" class="nav-link">Permissions</a></li>
-    <li><a href="/admin/license" class="nav-link">License</a></li>
-    <li><a href="/admin/labels" class="nav-link">Labels</a></li>
+<ul class="sidebar-top-level-items">
+    <li class="{{ Request::is('*users') ? 'active' : '' }}"><a href="/admin/users" class="nav-link">Users</a></li>
+    <li class="{{ Request::is('*roles') ? 'active' : '' }}"><a href="/admin/roles" class="nav-link">Roles</a></li>
+    <li class="{{ Request::is('*permissions') ? 'active' : '' }}"><a href="/admin/permissions" class="nav-link">Permissions</a></li>
+    <li class="{{ Request::is('*license') ? 'active' : '' }}"><a href="/admin/license" class="nav-link">License</a></li>
+    <li class="{{ Request::is('*labels') ? 'active' : '' }}"><a href="/admin/labels" class="nav-link">Labels</a></li>
 </ul>
--- resources/views/projects/elements/sidebar.blade.php
+++ resources/views/projects/elements/sidebar.blade.php
@@ -1,8 +1,8 @@
 <ul class="sidebar-top-level-items">
-    <li><a href="/projects/{{ $project->id }}" class="nav-link">Home</a></li>
-    <li><a href="/projects/{{ $project->id }}" class="nav-link">Files</a></li>
-    <li><a href="/projects/{{ $project->id }}/commits" class="nav-link">Commits</a></li>
-    <li><a href="/projects/{{ $project->id }}/branches" class="nav-link">Branches</a></li>
-    <li><a href="/projects/{{ $project->id }}/tags" class="nav-link">Tags</a></li>
-    <li><a href="/projects/{{ $project->id }}/issues" class="nav-link">Issues</a></li>
+    <li class="{{ Request::is('projects/' . $project->id) ? 'active' : '' }}"><a href="/projects/{{ $project->id }}" class="nav-link">Home</a></li>
+    <li class="{{ Request::is('projects/' . $project->id) ? 'active' : '' }}"><a href="/projects/{{ $project->id }}" class="nav-link">Files</a></li>
+    <li class="{{ Request::is('*commits') ? 'active' : '' }}"><a href="/projects/{{ $project->id }}/commits" class="nav-link">Commits</a></li>
+    <li class="{{ Request::is('*branches') ? 'active' : '' }}"><a href="/projects/{{ $project->id }}/branches" class="nav-link">Branches</a></li>
+    <li class="{{ Request::is('*tags') ? 'active' : '' }}"><a href="/projects/{{ $project->id }}/tags" class="nav-link">Tags</a></li>
+    <li class="{{ Request::is('*issues') ? 'active' : '' }}"><a href="/projects/{{ $project->id }}/issues" class="nav-link">Issues</a></li>
 </ul>
