a9c76d7f15aaed22a1ae3e896aea5ceb5e4c6603
May Meow <themaymeow@gmail.com>
2018-02-13 20:32:01 +0100

    Unit tests for Grup and Label

--- tests/Unit/GroupTest.php
+++ tests/Unit/GroupTest.php
@@ -7,19 +7,28 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Group;
 use App\User;
+use App\Project;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class GroupTest extends TestCase
 {
     use DatabaseMigrations;
     
-    /**
-     * @test
-     */
+    /** @test */
     public function group_pelongs_to_user()
     {
         $group = factory(Group::class)->create();
 
         $this->assertInstanceOf(User::class, $group->user);
+    }
+
+    public function group_can_have_project()
+    {
+        $group = factory(Group::class)->create();
+        $group->attachProject(faactory(Project::class)->create);
+
+        foreach ($group->projects as $project) {
+            $this->assertInstanceOf(Project::class, $project);
+        }
     }
 }
---
+++ tests/Unit/LabelTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Label;
+use App\Issue;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LabelTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    /** @test */
+    public function label_can_many_have_issues()
+    {
+        $label = factory(Label::class)->create();
+        $issues = factory(Issue::class)->create();
+
+        $label->issues()->save($issues);
+
+        foreach ($label->issues as $issue) {
+            $this->assertInstanceOf(Issue::class, $issue);
+        }
+        
+    }
+}
