e308358291a1d58d6130b9d07ffbc53a45a5cf7d
May <may+github@maymeow.click>
2018-03-16 13:56:30 +0100

    update tests

--- tests/Feature/Controller/LabelsTest.php
+++ tests/Feature/Controller/LabelsTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Label;
+use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class LabelsTest extends TestCase
@@ -16,13 +17,14 @@ class LabelsTest extends TestCase
     {
         parent::setUp();
 
+        $this->user = factory(User::class)->create();
         $this->label = factory(Label::class)->create();
     }
 
     /** @test */
     public function administrators_can_see_all_labels()
     {
-        $response = $this->get('/admin/labels');
+        $response = $this->actingAs($this->user)->get('/admin/labels');
         $response->assertStatus(200);
         $response->assertSee($this->label->title);
         $response->assertSee('Labels');
@@ -31,7 +33,7 @@ class LabelsTest extends TestCase
     /** @test */
     public function administrators_can_see_labels_create_form()
     {
-        $response = $this->get('/admin/labels/create');
+        $response = $this->actingAs($this->user)->get('/admin/labels/create');
         $response->assertStatus(200);
         $response->assertSee('Create label');
     }
