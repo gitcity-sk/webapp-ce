87092677a4d4aeb53632ba1337adb85fa09982b5
May Meow <themaymeow@gmail.com>
2018-03-18 17:32:05 +0100

    Update dependencies
    ADD ability to change App display name

--- config/webapp.php
+++ config/webapp.php
@@ -1,8 +1,28 @@
 <?php 
-
+/**
+ * Webapp config file
+ */
 return [
+
+    /**
+     * URL without https / https before address
+     * Is used to generate clone url for projects
+     */
     'url' => env('WEBAPP_URL', 'localhost'),
+
+    /**
+     * Port where is your SSH server running
+     * Requred for GIT server and clone url generation
+     */
     'ssh_port' => env('WEBAPP_SSH_PORT', ''),
+
+    /**
+     * Use this if you want have different homepage
+     */
     'redirect_home_page' => env('WEBAPP_HOME_REDIRECT', ''),
+
+    /**
+     * Enable or disable registration
+     */
     'allow_registration' => env('WEBAPP_ALLOW_REGISTRATION', '')
 ];
